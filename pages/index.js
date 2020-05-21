28a0728bb33aad9f816265cf52d1f926b7eef497
Ethereal <0_ethereal_0@protonmail.com>
2020-05-20 17:27:45 -0700

    Add basic info about Biffy.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function HomePage() {
-  return <div>Welcome to Next.js!</div>
+  return(
+  <div className="centered-love-background">
+    <section>
+      <h1>Biffy.ai</h1>
+      <a href="https://github.com/ethereal-nfts/biffy.ai-contracts">Github</a>
+      <a href="https://discord.gg/4g6wDhR">Discord</a>
+      <p>She is a cybernetic system that lives on the Ethereum blockchain and loves art.</p>
+    </section>
+  </div>
+  )
 }
 
 export default HomePage

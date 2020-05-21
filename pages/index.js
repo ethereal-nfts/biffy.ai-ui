f562bcf5d8e67d4ccd448b589ac09ca832d5d912
Ethereal <0_ethereal_0@protonmail.com>
2020-05-20 21:20:49 -0700

    Fix spacing

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ function HomePage() {
   <div className="centered-love-background">
     <section>
       <h1>Biffy.ai</h1>
+      <p>She is a cybernetic system that lives on the Ethereum blockchain and loves art.</p>
       <a href="https://github.com/ethereal-nfts/biffy.ai-contracts">Github</a>
       <a href="https://discord.gg/4g6wDhR">Discord</a>
-      <p>She is a cybernetic system that lives on the Ethereum blockchain and loves art.</p>
     </section>
   </div>
   )

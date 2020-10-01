7c6d2d15211953f682aa6aa381e0377fd7f122b4
Ethereal <0_ethereal_0@protonmail.com>
2020-09-30 21:43:19 -0700

    Add Uniswap link

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ function HomePage() {
       <p>She is a cybernetic system that lives on the Ethereum blockchain and loves art.</p>
       <a href="https://github.com/ethereal-nfts/biffy.ai-contracts">Github</a>
       <a href="https://discord.gg/4g6wDhR">Discord</a>
+      <a href="https://uniswap.info/pair/0xe97bFF521Dae652a1Bb107e2212907642c5A4AC4">Uniswap</a>
     </section>
   </div>
   )
